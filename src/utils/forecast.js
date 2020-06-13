8d6f84b8e5608b3ebf2be364634b362f93ef0cd7
tirlochanarora16 <tirlochan.arora16@gmail.com>
2020-06-13 19:22:51 +0530

    Added information about humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.success === false) {
             callback('Unable to find weather details for the given location. Try another search', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degrees out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degrees out.The humidity is ${body.current.humidity}.`)
         }
     })
 }  
